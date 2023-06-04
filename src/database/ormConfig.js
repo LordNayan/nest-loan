2856c4ab26dc1c71dfbd943a02c5adc19503796f
Nayan Lakhwani <nayanlakhwani@Nayans-MacBook-Pro.local>
2023-06-04 18:41:34 +0530

    chore: fixed migration

---
+++ src/database/ormConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  synchronize: false,
+  logging: false,
+  entities: ['./dist/database/entities/**/*.js'],
+  migrations: ['./dist/database/migrations/*.js'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+};
