2931fcb31c1152aec0acd95ec600ad1c6cc51a79
Nayan Lakhwani <nayanlakhwani@Nayans-MacBook-Pro.local>
2023-06-04 18:55:19 +0530

    chore: fixed migration

--- src/database/ormConfig.js
+++ src/database/ormConfig.js
@@ -3,6 +3,7 @@ module.exports = {
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   synchronize: false,
   logging: false,
